718d7cb7fcf44fff794ead96ba8fa2e992852834
kosmikus <kosmikus@gentoo.org>
2005-10-16 18:38:55 +0000

    added Keyword file

---
+++ Portage/Keyword.hs
@@ -0,0 +1,22 @@
+{-| Maintainer  :  Andres Loeh <kosmikus@gentoo.org>
+    Stability   :  provisional
+    Portability :  haskell98
+
+    Keyword utilities.
+-}
+
+module Portage.Keyword where
+
+import Data.Set
+
+type Keyword = String
+
+-- | Merges keywords; this is similar to merging USE flags
+mergeKeywords :: [Keyword] -> [Keyword] -> [Keyword]
+mergeKeywords xs []  =  xs
+mergeKeywords xs ys  =  elems $
+                        foldl (\s k -> if k == "-*"  then  empty
+                                                     else  insert k s)
+                              (fromList xs)
+                              ys
+
