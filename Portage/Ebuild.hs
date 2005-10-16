8de8762ccfe4d2232bfc66b8cc0d6ae30848a390
kosmikus <kosmikus@gentoo.org>
2005-10-16 22:00:31 +0000

    add tree locations to ebuilds

--- Portage/Ebuild.hs
+++ Portage/Ebuild.hs
@@ -27,8 +27,10 @@ import Portage.Keyword
 -- CDEPEND
 -- PDEPEND
 -- PROVIDE
+-- In addition, we store the tree in which the ebuild is located.
 
-data Ebuild = Ebuild  {  depend       ::  DepString,
+data Ebuild = Ebuild  {  location     ::  TreeLocation,
+                         depend       ::  DepString,
                          rdepend      ::  DepString,
                          slot         ::  String,
                          src_uri      ::  String,
@@ -46,21 +48,27 @@ data Ebuild = Ebuild  {  depend       ::  DepString,
   deriving (Show,Eq)
 
 
-getEbuild :: String -> Ebuild
-getEbuild e  |  length l <= 14  =  error "getEbuild: corrupted ebuild (too short)"
-             |  otherwise       =  Ebuild  (getDepString dep)
-                                           (getDepString rdep)
-                                           slt
-                                           src
-                                           (words restr)
-                                           home
-                                           lic
-                                           des
-                                           (splitKeywords key)
-                                           inh
-                                           (splitUse use)
-                                           (getDepString cdep)
-                                           (getDepString pdep)
-                                           (getDepAtom prov)
+data TreeLocation  =  Installed
+                   |  PortageTree  FilePath
+  deriving (Show,Eq)
+
+
+getEbuild :: TreeLocation -> String -> Ebuild
+getEbuild loc e  |  length l <= 14  =  error "getEbuild: corrupted ebuild (too short)"
+                 |  otherwise       =  Ebuild  loc
+                                               (getDepString dep)
+                                               (getDepString rdep)
+                                               slt
+                                               src
+                                               (words restr)
+                                               home
+                                               lic
+                                               des
+                                               (splitKeywords key)
+                                               inh
+                                               (splitUse use)
+                                               (getDepString cdep)
+                                               (getDepString pdep)
+                                               (getDepAtom prov)
   where l = lines e
         (dep:rdep:slt:src:restr:home:lic:des:key:inh:use:cdep:pdep:prov:_) = l
