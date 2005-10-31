44ca7be07c666e771fa67b9776b1e3c6805f2550
kosmikus <kosmikus@gentoo.org>
2005-10-31 12:46:39 +0000

    add tree generation to PortageConfig

--- Portage/PortageConfig.hs
+++ Portage/PortageConfig.hs
@@ -8,20 +8,43 @@
 module Portage.PortageConfig
   where
 
+import System.IO
+
 import Portage.Config
 import Portage.Tree
 import Portage.Use
 import Portage.UseDefaults
 
+data PortageConfig =  PortageConfig
+                        {
+                           config  ::  Config,
+                           tree    ::  Tree,
+                           inst    ::  Tree,
+                           itree   ::  Tree
+                        }
+
 -- | Portage configuration is read in the following order, in increasing priority:
 --   global < profile < user < environment < (package specific)
-portageConfig :: IO Config
-portageConfig =  do
-                     global    <-  getGlobalConfig
-                     profiles  <-  getProfileConfigs
-                     user      <-  getUserConfig
-                     env       <-  getEnvironmentConfig
-                     let merged = foldl1 mergeConfig (global : profiles ++ [user,env])
-                     inst      <-  createInstalledTree merged
-                     ud        <-  computeUseDefaults inst
-                     return (merged { use = arch merged : mergeUse (use merged) ud })
+portageConfig :: IO PortageConfig
+portageConfig = 
+    do
+        -- read basic configuration data
+        global    <-  getGlobalConfig
+        profiles  <-  getProfileConfigs
+        user      <-  getUserConfig
+        env       <-  getEnvironmentConfig
+        let merged  =  foldl1 mergeConfig (global : profiles ++ [user,env])
+        -- read installed tree, because that's required to determine virtuals
+        -- USE data
+        inst      <-  createInstalledTree merged
+        ud        <-  computeUseDefaults inst
+        -- the following is the "final" basic configuration
+        let cfg     =  merged { use = arch merged : mergeUse (use merged) ud }
+        -- now read the portage tree(s)
+        cats      <-  categories cfg
+        tree      <-  fixIO (\r ->  do
+                                        pt  <-  createTree cfg (portDir cfg) cats (eclasses r)
+                                        po  <-  mapM (\t -> createTree cfg t cats (eclasses r)) (overlays cfg)
+                                        return $ foldl overlayTree pt po)
+        let itree   =  overlayInstalledTree tree inst
+        return (PortageConfig cfg tree inst itree)
