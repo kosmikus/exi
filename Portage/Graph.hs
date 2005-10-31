ce1e0aba47f91b623aa5334b7bf8e27b48d4ef1a
kosmikus <kosmikus@gentoo.org>
2005-10-31 14:01:03 +0000

    graph generation (preliminary)

---
+++ Portage/Graph.hs
@@ -0,0 +1,37 @@
+{-| Maintainer  :  Andres Loeh <kosmikus@gentoo.org>
+    Stability   :  provisional
+    Portability :  haskell98
+
+    Generation of dependency graph.
+-}
+
+module Portage.Graph
+  where
+
+import Data.Graph.Inductive
+
+import Portage.Tree
+import Portage.Match
+import Portage.Dependency
+import Portage.Ebuild
+import Portage.Package
+import Portage.PortageConfig
+
+findVersions :: Tree -> DepAtom -> [Variant]
+findVersions = flip matchDepAtomTree
+
+showVariant :: Variant -> String
+showVariant (Variant m e)  =  showPV (pv m) ++ showLocation (location m) 
+                              ++ " " ++ unwords (map showMasked (masked m))
+
+showLocation :: TreeLocation -> String
+showLocation Installed = " (installed)"
+showLocation (PortageTree t) = " [" ++ t ++ "]"
+
+showMasked :: Mask -> String
+showMasked KeywordMasked = "(masked by keyword)"
+showMasked (HardMasked f r) = "(hardmasked in " ++ f ++ ")"
+showMasked (ProfileMasked f) = "(excluded from profile in " ++ f ++")"
+showMasked (Shadowed t) = "(shadowed by " ++ showLocation t ++ ")"
+
+
