c149dfbc40a338b881062d98e6cc15b39b5226dc
kosmikus <kosmikus@gentoo.org>
2005-11-09 20:16:13 +0000

    significant progress in graph generation

--- Portage/Graph.hs
+++ Portage/Graph.hs
@@ -9,6 +9,8 @@
 module Portage.Graph
   where
 
+import Data.Map (Map)
+import qualified Data.Map as M
 import Data.List (sortBy)
 import Data.Graph.Inductive hiding (version, Graph())
 import Control.Monad.Identity
@@ -17,7 +19,8 @@ import Control.Monad.State
 import Portage.Tree
 import Portage.Match
 import Portage.Dependency
-import Portage.Ebuild
+import Portage.Ebuild hiding (isAvailable)
+import qualified Portage.Ebuild as E
 import Portage.Package
 import Portage.PortageConfig
 import Portage.Config
@@ -50,7 +53,7 @@ data Selection  =  Accept   Variant
                 |  Reject   Failure
 
 data Failure  =  AllMasked Category Package [Variant]
-
+  deriving (Eq,Show)
 
 
 -- x :: Graph -> DepString -> Graph
@@ -59,48 +62,137 @@ data Failure  =  AllMasked Category Package [Variant]
 
 type DGraph = Graph
 type Graph = Gr Action DepType
-data DepType = Normal | Runtime | RPost
+data DepType = Normal | Runtime | Post | Meta
 
-data Action  =  Build      Variant
-             |  Available  Variant
-             |  Block      Variant
-             |  Fail       Failure
+data Action  =  Unavailable  Variant
+             |  Available    Variant
+             |  Unblocked    Variant
+             |  Build        Variant
+             |  Unblocking   Variant
+             |  Fail         Failure
+             |  Top
+
+-- | More efficient comparison for actions.
+instance Eq Action where
+  (Unavailable  v1)  ==  (Unavailable  v2)  =  pv (meta v1) == pv (meta v2)
+  (Available    v1)  ==  (Available    v2)  =  pv (meta v1) == pv (meta v2)
+  (Unblocked    v1)  ==  (Unblocked    v2)  =  pv (meta v1) == pv (meta v2)
+  (Build        v1)  ==  (Build        v2)  =  pv (meta v1) == pv (meta v2)
+  (Unblocking   v1)  ==  (Unblocking   v2)  =  pv (meta v1) == pv (meta v2)
+  Top                ==  Top                =  True
+  _                  ==  _                  =  False
+
+isAvailable :: Variant -> Map PV NodeInfo -> Bool
+isAvailable v m =  case M.lookup (pv (meta v)) m of
+                     Nothing  ->  False
+                     Just i   ->  active i
 
 data DepState =  DepState
                    {
                       pconfig  ::  PortageConfig,
                       dlocuse  ::  [UseFlag],
-                      graph    ::  Graph
+                      graph    ::  Graph,
+                      labels   ::  Map PV NodeInfo,
+                      counter  ::  Int
                    }
+
+data NodeInfo =  NodeInfo
+                   {  node     ::  Node,
+                      active   ::  Bool
+                   }
+
+unavailable  =  0
+available    =  1
+unblocked    =  2
+build        =  3
+unblocking   =  4
+
+offset       =  5
+
+at = (+)
 
 -- | Graph generation monad.
 type GG = State DepState
 
+-- | Modify the graph within the monad.
 modifyGraph :: (Graph -> Graph) -> GG ()
 modifyGraph f = modify (\s -> s { graph = f (graph s) })
 
-buildGraphForDepString :: DepString -> GG ()
-buildGraphForDepString = mapM_ buildGraphForDepTerm
+-- | Modify the counter within the monad.
+stepCounter :: GG Int
+stepCounter = do  c <- gets counter
+                  modify (\s -> s { counter = 5 + counter s })
+                  return c
 
-buildGraphForDepTerm :: DepTerm -> GG ()
+-- | Create a new node within the monad.
+newNode :: GG Node
+newNode =
+    do  g <- gets graph
+        return (head $ newNodes 1 g)
+
+-- | Insert a set of new nodes into the graph, if it doesn't exist yet.
+insNewNode :: Variant -> GG Node
+insNewNode v =
+    do  ls <- gets labels
+        let pv' = pv (meta v)
+        case M.lookup pv' ls of
+          Nothing ->  do  n <- stepCounter
+                          modifyGraph (insEdges [  (s,t,Meta)
+                                                |  (s,t) <- 
+                                                   [(unavailable  `at` n, available   `at` n),
+                                                    (available    `at` n, unblocked   `at` n),
+                                                    (unblocked    `at` n, build       `at` n),
+                                                    (build        `at` n, unblocked   `at` n)] ] .
+                                       insNodes [  (unavailable  `at` n, Unavailable  v),
+                                                   (available    `at` n, Available    v),
+                                                   (unblocked    `at` n, Unblocked    v),
+                                                   (build        `at` n, Build        v),
+                                                   (unblocking   `at` n, Unblocking   v)])
+                          return n
+          Just (NodeInfo n _) -> return n
+
+activate :: Variant -> GG ()
+activate v = modify (\s -> s { labels = M.update (\i -> Just $ i { active = True }) (pv (meta v)) (labels s) })
+
+-- | Builds a graph for the whole dependency string,
+--   by processing each term separately.
+buildGraphForDepString :: DepString -> GG [Node]
+buildGraphForDepString = fmap concat . mapM buildGraphForDepTerm
+
+-- | Builds a graph for the whole dependency string,
+--   and adds edges as specified by the given arguments.
+buildGraphForDepStringWith :: DepType -> Int -> Int -> Node -> DepString -> GG ()
+buildGraphForDepStringWith dt s t m ds =
+    do  ns <- buildGraphForDepString ds
+        modifyGraph (insEdges [(s `at` m, t `at` n, dt) | n <- ns])
+
+-- | Builds a graph for a single term.
+--   If it is an atom, just delegate.
+--   If it is a use-qualified thing, check the USE flag and delegate.
+--   If it is an or-dependency, ...
+buildGraphForDepTerm :: DepTerm -> GG [Node]
 buildGraphForDepTerm dt =
     do  luse <- gets dlocuse
         case dt of
-          Plain d                   ->  buildGraphForDepAtom d
+          Plain d                   ->  fmap (:[]) (buildGraphForDepAtom d)
           Use n f ds
             | n /= (f `elem` luse)  ->  buildGraphForDepString ds
-            | otherwise             ->  return ()
+            | otherwise             ->  return []
 
-buildGraphForDepAtom :: DepAtom -> GG ()
+buildGraphForDepAtom :: DepAtom -> GG Node
 buildGraphForDepAtom da =
-    do  pc <- gets pconfig
+    do  pc  <-  gets pconfig
+        g   <-  gets graph
+        ls  <-  gets labels
         let s          =  strategy pc
             t          =  itree pc
             (cat,pkg)  =  catpkgFromDepAtom da
         case sselect s cat pkg (findVersions t da) of
           Accept v@(Variant m e)  ->  
-            let  available  =  isAvailable (location m)
-                 stop       =  available && sstop s v -- if it's an installed ebuild, we can decide to stop here!
+            let  avail      =  E.isAvailable (location m)  -- installed or provided?
+                 already    =  isAvailable v ls
+                 stop       =  already || avail && sstop s v 
+                                 -- if it's an installed ebuild, we can decide to stop here!
             in                     let  rdeps    =  rdepend  e
                                         deps     =  depend   e
                                         pdeps    =  pdepend  e
@@ -108,14 +200,16 @@ buildGraphForDepAtom da =
                                    in   -- set new local USE context
                                         withState (\s -> s { dlocuse = luse }) $
                                         do
+                                            -- insert nodes for v, and activate
+                                            n <- insNewNode v
+                                            activate v
                                             -- add deps to graph
-                                            buildGraphForDepString deps
-                                            -- add "build v" to graph
-                                            -- add "remove v of same slot (if exists)" to graph
+                                            buildGraphForDepStringWith Normal build available n deps
                                             -- add rdeps to graph
-                                            buildGraphForDepString rdeps
+                                            buildGraphForDepStringWith Runtime available available n rdeps
                                             -- add pdeps to graph
-                                            buildGraphForDepString pdeps
+                                            buildGraphForDepStringWith Runtime available build n pdeps
+                                            return n
 
 strategy :: PortageConfig -> Strategy
 strategy = const updateStrategy
