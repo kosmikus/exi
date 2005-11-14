458c78d93c57a2a3ca31353b6b01804cb874186e
kosmikus <kosmikus@gentoo.org>
2005-11-14 08:16:09 +0000

    refactoring of readProfileFile

--- Portage/Profile.hs
+++ Portage/Profile.hs
@@ -9,8 +9,12 @@
 module Portage.Profile
   where
 
+import Prelude hiding (catch)
+import System.Posix
 import System.Directory
 import System.IO.Unsafe
+import Control.Monad
+import Control.Exception
 
 import Portage.Constants
 import Portage.Utilities
@@ -20,21 +24,11 @@ import Portage.Utilities
 --   the file. The resulting list contains the result for each of the 
 --   found files, the file with highest priority last.
 readProfileFile :: FilePath -> (FilePath -> IO a) -> IO [a]
-readProfileFile f parser =  unsafeInterleaveIO $ 
-                            do  -- read local profile (with priority)
-                                acc <- readProfileIn localProfileDir [] f parser
-                                -- read selected profile
-                                readProfileFileFrom profileDir acc f parser
-
-readProfileFileFrom :: FilePath -> [a] -> FilePath -> (FilePath -> IO a) -> IO [a]
-readProfileFileFrom pdir acc f parser =
-  do  new_acc     <-  readProfileIn pdir acc f parser
-      -- is there a parent?
-      let p_local = pdir ./. profileParent
-      p_exists    <-  doesFileExist p_local
-      if p_exists  then  do  p_dir <- strictReadFile p_local >>= return . stripNewlines . stripComments
-                             readProfileFileFrom (pdir ./. p_dir) new_acc f parser
-                   else  return new_acc
+readProfileFile f parser =
+  unsafeInterleaveIO $
+  do
+      pdirs <- getProfileDirs
+      fmap reverse $ foldM (\acc d -> readProfileIn d acc f parser) [] pdirs
 
 readProfileIn :: FilePath -> [a] -> FilePath -> (FilePath -> IO a) -> IO [a]
 readProfileIn pdir acc f parser =
@@ -46,15 +40,15 @@ readProfileIn pdir acc f parser =
       return $ f_contents ++ acc
 
 getProfileDirs :: IO [FilePath]
-getProfileDirs =  unsafeInterleaveIO $
-                    getProfileDirsFrom profileDir [localProfileDir]
+getProfileDirs =  unsafeInterleaveIO $ getProfileDirsFrom profileDir [localProfileDir]
 
 getProfileDirsFrom :: FilePath -> [FilePath] -> IO [FilePath]
 getProfileDirsFrom pdir acc =
-  do  let new_acc = pdir : acc
+  do  pdir' <- canonicalizePath pdir
+      let new_acc = pdir' : acc
       -- is there a parent?
-      let p_local = pdir ./. profileParent
+      let p_local = pdir' ./. profileParent
       p_exists <- doesFileExist p_local
       if p_exists  then  do  p_dir <- strictReadFile p_local >>= return . stripNewlines . stripComments
-                             getProfileDirsFrom (pdir ./. p_dir) new_acc
+                             getProfileDirsFrom (pdir' ./. p_dir) new_acc
                    else  return new_acc
