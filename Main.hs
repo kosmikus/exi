fc80080095514ef913ebf23924ba8d08df3a47a9
kosmikus <kosmikus@gentoo.org>
2005-12-13 14:15:43 +0000

    added a command line user interface

--- Main.hs
+++ Main.hs
@@ -1,92 +1,29 @@
 module Main where
 
 import System.Environment
-import Data.Graph.Inductive hiding (context)
 import qualified Data.Map as M
-import Data.Maybe (fromJust)
-import Control.Monad.State
 
 import Portage.Graph
-import Portage.GraphUtils
 import Portage.Dependency
 import Portage.PortageConfig
-import Portage.Strategy
-import Portage.Ebuild hiding (rdepend)
 import Portage.Package
 import Portage.Tree
-import Portage.Config
+import Portage.Merge
+import Portage.Interface
 
-main = do  [a] <- getArgs
-           main' a
+main = do  args <- getArgs
+           handleArgs args
            
-
 main' d = 
     do  x <- portageConfig
         putStr $ unlines $ map (showVariant (config x)) $ findVersions (itree x) (getDepAtom d)
 
 
-pretend' b v d = 
-    do  x <- fmap (if b then (\x -> x { strategy = updateStrategy }) else id ) portageConfig
-        let initialState =  DepState
-                              {
-                                 pconfig   =  x,
-                                 dlocuse   =  [],
-                                 graph     =  insNodes [(top,Top),(bot,Bot)] empty,
-                                 labels    =  M.empty,
-                                 active    =  M.empty,
-                                 counter   =  max top bot + 1,
-                                 callback  =  rdepend (top,top,top)
-                              }
-        let fs = runState (buildGraphForUDepString (getDepString d)) initialState 
-        putStr $ "Calculating dependencies: "
-        when v $ putStrLn ""
-        putStr $ concatMap (showProgress v) $ fst fs
-        putStrLn $ "\nGraph complete: "
-        let gr = graph $ snd $ fs
-        let mergelist = postorderF $ dffWith lab' [0] $ gr
-        putStr $ unlines $ map show $ mergelist
-        putStrLn $ "\nShort version: "
-        putStr $ unlines  $  map (showAction (config x)) 
-                          $  filter (\ a -> case a of Built _ -> True; _ -> False) $ mergelist
-        let cycles = cyclesFrom gr [top]
-        when (not (null cycles)) $
-            do
-                putStrLn "\nThe graph has cycles:" 
-                putStr $ unlines $ map (unlines . map (showNode v gr)) cycles
-        return gr
 
-showNode :: Bool -> DGraph -> Int -> String
-showNode v gr n = (show . fromJust . lab gr $ n) ++ number
-  where number = if v then " [" ++ show n ++ "]" else ""
-
-showProgress :: Bool -> Progress -> String
-showProgress True = showProgressLong
-showProgress False = showProgressShort
-
-showProgressLong (LookAtEbuild pv o) = showPV pv ++ " " ++ showOriginLong o ++ "\n"
-showProgressLong (AddEdge n1 n2 d)   = "added edge " ++ show n1 ++ " " ++ show n2 ++ " " ++ show d ++ "\n"
-showProgressLong (Message s)         = s ++ "\n"
-
-showProgressShort (LookAtEbuild pv o) = showOriginShort o
-showProgressShort (AddEdge _ _ _)     = ""
-showProgressShort (Message s)         = ""
-
-showOriginLong FromCache         =  "(from cache)"
-showOriginLong CacheRegen        =  "(regenerated cache entry)"
-showOriginLong EclassDummy       =  "(made eclass dummy)"
-showOriginLong FromInstalledDB   =  "(available)"
-showOriginLong IsProvided        =  "(provided)"
-
-showOriginShort FromCache        =  "."
-showOriginShort CacheRegen       =  "C"
-showOriginShort EclassDummy      =  "E"
-showOriginShort FromInstalledDB  =  "i"
-showOriginShort IsProvided       =  "p"
-
-ep   = pretend' False False
-eup  = pretend' True  False
-epv  = pretend' False True
-eupv = pretend' True  True
+ep   = pretend $ MergeState False False
+eup  = pretend $ MergeState True  False
+epv  = pretend $ MergeState False True
+eupv = pretend $ MergeState True  True
 
 
 -- expand function, too slow:
---
+++ Portage/Interface.hs
@@ -0,0 +1,68 @@
+{-|
+    Maintainer  :  Andres Loeh <kosmikus@gentoo.org>
+    Stability   :  provisional
+    Portability :  haskell98
+
+    User interface.
+-}
+
+module Portage.Interface
+  where
+
+import System.Console.GetOpt
+
+import Portage.Merge
+
+data Command a =  Command
+                    {
+                      command      ::  [String],
+                      description  ::  String,
+                      state        ::  a,
+                      options      ::  [OptDescr (a -> a)],
+                      handler      ::  a -> [String] -> IO ()
+                    }
+
+data Command' = forall a. Command' (Command a)
+
+commands :: [Command']
+commands =  [Command' mergeCmd]
+
+mergeCmd :: Command MergeState
+mergeCmd =  Command
+              {
+                command = ["merge"],
+                description = "merge one or more variants",
+                state = MergeState { mupdate = False, mverbose = False },
+                options = mergeOpts,
+                handler = doMerge
+              }
+
+mergeOpts :: [OptDescr (MergeState -> MergeState)]
+mergeOpts = [Option "u" ["update"] (NoArg (\s -> s { mupdate = True })) "update variants",
+             Option "v" ["verbose"] (NoArg (\s -> s { mverbose = True })) "be verbose"]
+
+handleArgs :: [String] -> IO ()
+handleArgs []      =  printGlobalHelp
+handleArgs [a]
+  | isHelp a       =  printGlobalHelp
+handleArgs (x:xs)  =  case findCommand x of
+                        Nothing -> handleCommand  (Command' mergeCmd)  (x:xs)
+                        Just c  -> handleCommand  c                    xs
+
+isHelp :: String -> Bool
+isHelp "-?"      =  True
+isHelp "-h"      =  True
+isHelp "--help"  =  True
+isHelp _         =  False
+
+findCommand :: String -> Maybe Command'
+findCommand x = lookup x [ (n,c') | c'@(Command' c) <- commands, n <- command c ] 
+
+printGlobalHelp = putStrLn "<global help>"
+
+handleCommand :: Command' -> [String] -> IO ()
+handleCommand (Command' c) args =  
+    let (fs,n,es)  =  getOpt Permute (options c) args
+    in  case es of
+          []  ->  handler c (foldl (flip ($)) (state c) fs) n
+          _   ->  putStrLn (unlines es) >> putStrLn (usageInfo "" . options $ c)
